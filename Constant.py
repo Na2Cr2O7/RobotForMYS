edd9c7f9c344baa303711f955868b8a72712222a
Na2Cr2O7 <148955755+Na2Cr2O7@users.noreply.github.com>
2024-10-25 21:29:17 +0800

    Fatal Problem

--- Constant.py
+++ Constant.py
@@ -4,22 +4,20 @@ Constant.py
 '''
 
 MAXREPLYCOUNT=300 # 最大回复数
-NYANAME='氯铂酸喵Nya2PtCl6' # 机器人昵称
-VERSION='1.11.10T' # 版本号
-ACCOUNT="shanouyan158@163.com" # 邮箱账号
-PASSWORD="yuan5520520" # 邮箱密码
+NYANAME='' # 机器人昵称
+VERSION='' # 版本号
+with open('account.txt','r',encoding='utf-8') as f:
+    ACCOUNT,PASSWORD=f.read().strip().split() # 账号密码
 
-MODELSBYDAY=['qwen2.5:1.5b','qwen2:1.5b','qwen:1.8b','qwen2.5:0.5b','qwen2:0.5b'] # 日常模式模型
-MODELSBYNIGHT=['qwen2.5:0.5b','qwen2:0.5b'] # 夜间模式模型
+MODELSBYDAY=[] # 日常模式模型
+MODELSBYNIGHT=['qwen2.5:0.5b',] # 夜间模式模型
 MODELSATDAY=MODELSBYDAY
 MODELSATNIGHT=MODELSBYNIGHT
 
 VISIONMODEL='moondream' # 视觉模型
 DOTRANSLATE=True # 是否翻译
 
-ModalWords=['呢','哦','喵','呀']#语气词
+ModalWords=['呢','呢']#语气词
 MODALWORDS=ModalWords
-EMOTIONS=['_(香菱-诶嘿嘿)','_(香菱-星星眼)','_(香菱-新菜谱)',
-          '_(芙宁娜-好震惊！)','_(芙宁娜-好困难)','_(芙宁娜-好好笑)'
-          ,'_(芙宁娜-好无聊)','_(芙宁娜-乐)','_(香菱-好吃)'] # 表情列表
-VIDEOURL='https://space.bilibili.com/3546570641377347/video' # 视频地址
+EMOTIONS=['_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)','_(香菱-诶嘿嘿)'] # 表情列表
+VIDEOURL='https://space.bilibili.com/297786973' # 视频主页地址
